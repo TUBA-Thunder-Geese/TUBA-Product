e88dd025f3669e486a2f91d2297d618df4a73994
[Emi Knox-Hershey] <emiluzartist@gmail.com>
2024-05-02 16:49:56 -0400

    added new webpack and package.json

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
-import React from "react";
+import React from 'react';
 import { createRoot } from 'react-dom/client';
-import App from "./App";
+import App from './App.jsx'
 
-const root = createRoot(document.getElementById('root'))
 
-root.render(<App />)
+import './style.css';
+
+const root = createRoot(document.getElementById('root'));
+
+root.render(
+    <div>
+        <App />
+    </div>
+
+);
