12835b98d0e8b4b423cd0e4297b8a0bc279685db
[Emi Knox-Hershey] <emiluzartist@gmail.com>
2024-05-02 17:43:03 -0400

    changed containers folder to pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 import { Route, Router, Routes } from "react-router";
-import Home from "./components/Home";
-import Download from "./components/ProductDownload";
+import Home from "./pages/Home";
+import Download from "./pages/ProductDownload";
 import Nav from "./components/Nav";
 
 export default function App() {
 
-  console.log('TUBA TRACING FOR EVAHHH')
 
   return (
     <>
