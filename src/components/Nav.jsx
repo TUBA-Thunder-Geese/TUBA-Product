ae2933d6b6d2439e05377f201da379f1b9dfe24e
[Emi Knox-Hershey] <emiluzartist@gmail.com>
2024-05-05 14:15:48 -0400

    added styling nav buttons and logo image

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -6,10 +6,17 @@ export default function Nav() {
 //   const navigate = useNavigate();
   return (
     <div className="nav-wrapper">
-      <div className="nav">
-        <ul>
-          <li>Product</li>
-          <li>Docs</li>
+      <div class="flex justify-start">
+        <img src={tubaLogo}></img>
+      </div>
+      <div className="nav-buttons">
+        <ul class='flex justify-end custom-space'>
+          <li class='mr-3'>
+            <a class="inline-block border border-rgb(91, 143, 185)-500 rounded py-1 px-3 bg-500 text-white hover:bg-nav-blue" href="#">Product</a>
+          </li>
+          <li>
+            <a class="inline-block border border-rgb(91, 143, 185)-500 rounded py-1 px-3 bg-500 text-white hover:bg-nav-blue" href="#">Docs</a>
+          </li>
         </ul>
       </div>    
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'nav-blue': 'rgb(91, 143, 185)',
+      }
+    },
   },
   plugins: [],
 }
