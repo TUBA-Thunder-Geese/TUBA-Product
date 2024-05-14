e0822085f2527074999e3ac53e8f4dc4462c6289
[Emi Knox-Hershey] <emiluzartist@gmail.com>
2024-05-13 20:18:45 -0400

    added heatmap img

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,8 +4,14 @@ import React from "react";
 export default function Home() {
 
   return (
-    <div className="home-wrapper">
-      <h1>Home stuff goes here</h1>
+    <div className="home-wrapper flex justify-center">
+      <div className="title-section">
+        <h1>TUBA</h1>
+        <p>Error tracing made simple in micro service architecture</p>
+      </div>
+      <div>
+        <img className="heat-map-img" src={HeatMapSS}></img>
+      </div>
     </div>
   )
 }
