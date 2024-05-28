93f95ef96880afb794e8821accea5e308e8f29d7
[Emi Knox-Hershey] <emiluzartist@gmail.com>
2024-05-28 13:05:41 -0400

    added logo images and deleted bundle

--- src/pages/ProductDownload.jsx
+++ src/pages/ProductDownload.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 
-import TitleContain from "../components/TitleContain.jsx";
+import TitleContain from '../components/TitleContain.jsx';
+import ImgContain from '../components/ImgContain.jsx';
 
+import npmLogo from '../images/npm-logo.png';
+import dockerLogo from '../images/dockerhub-logo.png';
 
 export default function Download() {
     return (
@@ -12,9 +15,11 @@ export default function Download() {
         <div className="options-grid">
           <section className="npm-section">
             <TitleContain text={"NPM Package"}/>
+            <ImgContain src={"npmLogo"} alt={"image of NPM logo with link to website"} />
           </section>
           <section className="docker-section">
             <TitleContain text={"Docker Image"}/>
+            <ImgContain src={"dockerLogo"} alt={"image of dockerHub logo with link to website"}/>
           </section>
         </div>
       </div>
