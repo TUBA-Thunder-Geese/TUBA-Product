452ad27479dab10177609e8e1ad2090f81696b9c
Myles Austin <mtv1243@gmail.com>
2024-06-26 20:13:20 -0400

    fix spacing on home and download pages

--- src/components/TextContain.jsx
+++ src/components/TextContain.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 
 
-export default function TextContain ({text}) {
+export default function TextContain ({text, sectionClasses, divClasses, pClasses}) {
 
   return (
-    <section className="text-section">
-      <div className="text-container">
-        <p className="text">{text}</p>
+    <section className={"text-section " + (sectionClasses ? sectionClasses : '')}>
+      <div className={"text-container " + (divClasses ? divClasses: '')}>
+        <p className={"text " + (pClasses ? pClasses : '')}>{text}</p>
       </div>
     </section>
   )
